6fe29483331fa694b1ef5228fdab43a230bca98c
M Rohmatullah <m.rohmatullah10@gmail.com>
2020-08-07 18:05:19 +0700

    test

---
+++ app/src/main/java/com/mycorp/androidchallengethree/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.mycorp.androidchallengethree.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mycorp.androidchallengethree.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/mycorp/androidchallengethree/shoppinglist/ShoppingActivity.java
@@ -0,0 +1,16 @@
+package com.mycorp.androidchallengethree.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mycorp.androidchallengethree.R;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+    }
+}
